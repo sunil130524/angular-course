456cdb0115c86f65f2eb65fa920a82d2293ffea6
Sunil Kumar C <sunil130524@gmail.com>
2024-09-21 21:07:46 +0530

    Create new component and use Input decorator to pass data to the child component

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+  @Input({
+    required: true
+  })
+  course: Course
+
+}
